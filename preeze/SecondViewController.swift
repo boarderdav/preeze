25a0c6764ec125126174d19eeb42057d2ec36a93
David Jenkins <david.g.jenkins@nasa.gov>
2016-05-18 16:26:55 -0400

    committing first repo to remote directory

---
+++ preeze/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  preeze
+//
+//  Created by Jenkins, David Glenn (LARC-B7)[LITES] on 5/18/16.
+//  Copyright © 2016 RocketScience. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
